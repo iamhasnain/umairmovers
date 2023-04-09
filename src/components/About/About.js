8dac0f7e621a32b28481cd109b613c180a220c54
tv2020 <techversatile@gmail.com>
2023-04-10 01:47:16 +0500

    re initialize all resorces

---
+++ src/components/About/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <div id="about" className="About-section">
+      <h1>About us</h1>
+      <p1>
+      Umair Movers is the first Company with an advanced and organized transport system in Sindh. It is the only transport company to have a province wide network with various  links. Being a transport service provider the company owns a large fleet of buses, Technically Advanced Workshops to maintain the fleet for the safety of the passengers, State of the Art Terminals, Training Institutes for Drivers and Mechanical Staff.
+      </p1>
+      
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Contact.css';
+
+const Contact = () => {
+  return (
+    <div id="contact" className="contact-section">
+    </div>
+  );
+};
+
+export default Contact;
