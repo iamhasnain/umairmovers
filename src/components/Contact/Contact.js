ee9035f416dd8260f7f4284641e7e86d056319cd
tv2020 <techversatile@gmail.com>
2023-04-09 05:30:21 +0500

    comit code

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,8 +4,8 @@ import './Contact.css';
 const Contact = () => {
   return (
     <div id="contact" className="contact-section">
-      <h1>Welcome to My One Page Website</h1>
-      // Add more content here...
+      {/* <h1>Welcome to My One Page Website</h1> */}
+      
     </div>
   );
 };
